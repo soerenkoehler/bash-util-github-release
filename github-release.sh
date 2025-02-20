17fa75ec4296992fbc23e666060518af4dfe830a
Sören Köhler <soerenkoehler@users.noreply.github.com>
2025-02-20 12:40:35 +0100

    create release with gh cli

--- github-release.sh
+++ github-release.sh
@@ -7,7 +7,7 @@ if [[ $GITHUB_REF_TYPE == 'tag' ]]; then
     printf "create stable release %s (will fail if exists)\n" $GITHUB_REF_NAME
     gh release create --verify-tag $GITHUB_REF_NAME ./dist/*
 elif [[ $GITHUB_REF_TYPE == 'branch' ]]; then
-    printf "create nightly release (will delete old release if exists)\n"
+    printf "create nightly release on branch %s (will delete old release if exists)\n" $GITHUB_REF_NAME
     gh release delete --cleanup-tag --yes nightly ||\
-    gh release create --target $GITHUB_REF_NAME nightly ./dist/*
+    gh release create --target $GITHUB_REF nightly ./dist/*
 fi
