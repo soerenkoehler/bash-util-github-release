8d25e6281e9e341abf804e98c2850354fc1b9e00
Sören Köhler <soerenkoehler@users.noreply.github.com>
2025-02-20 12:54:47 +0100

    create release with gh cli

--- github-release.sh
+++ github-release.sh
@@ -4,10 +4,31 @@ printf "verify github auth status:\n"
 gh auth status
 
 if [[ $GITHUB_REF_TYPE == 'tag' ]]; then
+
     printf "create stable release %s (will fail if exists)\n" $GITHUB_REF_NAME
-    gh release create --verify-tag $GITHUB_REF_NAME ./dist/*
+
+    RELEASE=$GITHUB_REF_NAME
+    gh release create \
+        -t $RELEASE \
+        --verify-tag \
+        $RELEASE
+
 elif [[ $GITHUB_REF_TYPE == 'branch' ]]; then
+
     printf "create nightly release on branch %s (will delete old release if exists)\n" $GITHUB_REF_NAME
-    gh release delete --cleanup-tag --yes nightly ||\
-    gh release create --target $GITHUB_REF nightly ./dist/*
+
+    RELEASE=nightly
+    gh release delete \
+        --cleanup-tag \
+        --yes \
+        "$RELEASE" \
+    || gh release create \
+        -t "Nightly-$(date -I)" \
+        --target $GITHUB_REF \
+        --latest false
+        $RELEASE
+fi
+
+if [[ -e ./dist ]]; then
+    printf "found ./dist folder => uploading artifacts to %s\n" $RELEASE
 fi
