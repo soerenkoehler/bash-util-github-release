58879d693a212fef6b1a6f3fc68c1b1da13f7262
Sören Köhler <soerenkoehler@users.noreply.github.com>
2025-02-20 11:12:32 +0100

    create release with gh cli

---
+++ github-release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+printf "verify github auth status:\n"
+gh auth status
+
+# VERSION="v$(grep -E '^version = "(.+)"' Cargo.toml | sed 's/.\+"\(.\+\)"/\1/')"
+# gh release create "$VERSION"
+
+printf "\ntrigger action:\n%s\n%s\n%s\n" \
+    $GITHUB_REF \
+    $GITHUB_REF_NAME \
+    $GITHUB_REF_TYPE
+
+if [[ $GITHUB_REF_TYPE == 'tag' ]]; then
+    printf "TODO: create stable release\n"
+elif [[ $GITHUB_REF_TYPE == 'branch' ]]
+    printf "TODO: create nightly release\n"
+fi
+
+printf "TODO: upload artifacts\n"
